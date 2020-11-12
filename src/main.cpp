af6a60653adf44a8ef4b656052cb610d4fdd17ca
FlavianMF <flavianmelquiades@gmail.com>
2020-11-12 10:50:26 -0300

    LDR

--- src/main.cpp
+++ src/main.cpp
@@ -12,4 +12,21 @@ void setup() {
   steps();
 }
 
-void loop() {}
+uint16_t rawLightLevel = 0;
+
+void loop() {
+  rawLightLevel = analogRead(ldrPin);
+  if (rawLightLevel <= 342) {
+    analogWrite(greenLedPin, map(rawLightLevel, 0, 341, 0, 255));
+    analogWrite(yellowLedPin, 0);
+    analogWrite(redLedPin, 0);
+  } else if (rawLightLevel > 684) {
+    analogWrite(greenLedPin, 255);
+    analogWrite(yellowLedPin, 255);
+    analogWrite(redLedPin, map(rawLightLevel, 685, 1024, 0, 255));
+  } else {
+    analogWrite(greenLedPin, 255);
+    analogWrite(yellowLedPin, map(rawLightLevel, 342, 684, 0, 255));
+    analogWrite(redLedPin, 0);
+  }
+}
