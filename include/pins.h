00838e1ca8547e0e8cd1632ac198a10c7ace438c
FlavianMF <flavianmelquiades@gmail.com>
2020-11-12 09:24:42 -0300

    Definição dos pinos

---
+++ include/pins.h
@@ -0,0 +1,6 @@
+#define greenLedPin 9
+#define redLedPin 5
+#define yellowLedPin 10
+
+#define buzzerPin 13
+#define ldrPin 18
--- src/main.cpp
+++ src/main.cpp
@@ -1,9 +1,7 @@
 #include <Arduino.h>
 
-void setup() {
-  // put your setup code here, to run once:
-}
+#include "pins.h"
 
-void loop() {
-  // put your main code here, to run repeatedly:
-}
+void setup() {}
+
+void loop() {}
